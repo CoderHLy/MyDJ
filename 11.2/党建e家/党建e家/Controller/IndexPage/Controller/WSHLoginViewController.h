82406a7b96ba578d81abf0076ff269b5daed07a1
tongry <960570373@qq.com>
2018-11-12 14:33:41 +0800

    测试提交

---
+++ 11.2/党建e家/党建e家/Controller/IndexPage/Controller/WSHLoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  WSHLoginViewController.h
+//  党建e家
+//
+//  Created by OC on 2018/11/12.
+//  Copyright © 2018年 何龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WSHLoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 11.2/党建e家/党建e家/Controller/IndexPage/Controller/WSHLoginViewController.m
@@ -0,0 +1,32 @@
+//
+//  WSHLoginViewController.m
+//  党建e家
+//
+//  Created by OC on 2018/11/12.
+//  Copyright © 2018年 何龙. All rights reserved.
+//
+
+#import "WSHLoginViewController.h"
+
+@interface WSHLoginViewController ()
+
+@end
+
+@implementation WSHLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
